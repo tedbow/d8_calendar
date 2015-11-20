4e50b353b5e5890d0316d0e3c67cf970bf4a947d
Ted Bowman <ted.git@tedbow.com>
2015-11-20 16:33:58 +0000

    Create new submodule calendar_datetime and added Views Argument classes from https://www.drupal.org/files/issues/can_t_select-2567815-13.patch

---
+++ calendar_datetime/src/Plugin/views/argument/FullDate.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\calendar_datetime\Plugin\views\argument\FullDate.
+ */
+
+namespace Drupal\calendar_datetime\Plugin\views\argument;
+use Drupal\datetime\Plugin\views\Argument\Date;
+
+/**
+ * Argument handler for a day.
+ *
+ * @ViewsArgument("datetime_full_date")
+ */
+class FullDate extends Date {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $argFormat = 'Ymd';
+
+}
