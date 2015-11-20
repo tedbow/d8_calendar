fe0d3f8017e82e3967b579020937176a0caa613d
Ted Bowman <ted.git@tedbow.com>
2015-11-20 16:33:58 +0000

    Create new submodule calendar_datetime and added Views Argument classes from https://www.drupal.org/files/issues/can_t_select-2567815-13.patch

---
+++ calendar_datetime/src/Plugin/views/argument/WeekDate.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\calendar_datetime\Plugin\views\argument\WeekDate.
+ */
+
+namespace Drupal\calendar_datetime\Plugin\views\argument;
+use Drupal\datetime\Plugin\views\Argument\Date;
+
+/**
+ * Argument handler for a day.
+ *
+ * @ViewsArgument("datetime_week")
+ */
+class WeekDate extends Date {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $argFormat = 'W';
+
+}
---
+++ calendar_datetime/src/Plugin/views/argument/YearMonthDate.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\calendar_datetime\Plugin\views\argument\YearMonthDate.
+ */
+
+namespace Drupal\calendar_datetime\Plugin\views\argument;
+use Drupal\datetime\Plugin\views\Argument\Date;
+
+/**
+ * Argument handler for a day.
+ *
+ * @ViewsArgument("datetime_year_month")
+ */
+class YearMonthDate extends Date {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $argFormat = 'Ym';
+
+}
