08cd5658690563430da64f21ad99ca7b6720903e
Ted Bowman <ted.git@tedbow.com>
2015-11-26 14:50:42 +0000

    test pager twig

---
+++ src/Plugin/views/pager/CalendarPager.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\calendar\Plugin\views\pager\CalendarPager.
+ */
+
+
+namespace Drupal\calendar\Plugin\views\pager;
+
+
+use Drupal\views\Plugin\views\pager\PagerPluginBase;
+
+/**
+ * The plugin to handle full pager.
+ *
+ * @ingroup views_pager_plugins
+ *
+ * @ViewsPager(
+ *   id = "calendar",
+ *   title = @Translation("Calendar Pager"),
+ *   short_title = @Translation("Calendar"),
+ *   help = @Translation("Calendar Pager"),
+ *   theme = "calendar_pager",
+ * )
+ */
+class CalendarPager extends PagerPluginBase {
+  public function render($input) {
+    return array(
+      '#theme' => $this->themeFunctions(),
+      '#myvalue' => 'override value',
+    );
+  }
+
+}
